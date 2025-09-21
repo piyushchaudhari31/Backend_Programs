3f78cc2b7af12db1bdcffeca369993d7fcf85ab2
piyushchaudhari31 <mrpiyushchaudhari2006@gmail.com>
2025-09-21 11:36:02 +0530

    Navigate button

--- Burger website/frontend/src/pages/Components/Burger.jsx
+++ Burger website/frontend/src/pages/Components/Burger.jsx
@@ -3,8 +3,16 @@ import burger_img from '../../photo/Burger_img.png'
 import '../../css/burger.css'
 import Nav from '../../Navbar/Nav'
 import video from '../../photo/buregr_vid.mp4'
+import { useNavigate } from 'react-router-dom'
 
 const Burger = () => {
+
+  const navigate = useNavigate()
+
+  const navigateHandler = ()=>{
+    navigate('/foodmenu')
+  }
+
   return (
     <div className='Burger'>
       <div className="vidio">
@@ -21,7 +29,7 @@ const Burger = () => {
             <img src={burger_img} alt="" />
           </div>
           <div className='burger_btn'>
-            <button className="btn">SEE MORE</button>
+            <button className="btn" onClick={navigateHandler}>SEE MORE</button>
           </div>
         </div>
 
